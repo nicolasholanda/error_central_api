2e854cf452f8ee04020f102bd97343197a0fcff6
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-06-05 20:53:30 -0300

    Adicionando urls para acesso aos logs e para autenticação

--- errorcentral/urls.py
+++ errorcentral/urls.py
@@ -1,21 +1,18 @@
-"""errorcentral URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
+from errorcentralapp import views
+
+router = routers.DefaultRouter()
+router.register(r'logs', views.ErrorLogList)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ]
