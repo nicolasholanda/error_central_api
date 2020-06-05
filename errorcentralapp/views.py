e3a8facea480a117840751a465aea14e0064d2c9
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-06-05 20:55:58 -0300

    Implementando view para operações de CRUD autenticadas

--- errorcentralapp/views.py
+++ errorcentralapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
 
-# Create your views here.
+from errorcentralapp.filters import ErrorLogFilterSet
+from .models import ErrorLog
+from . import serializers
+
+
+class ErrorLogList(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   GenericViewSet):
+    queryset = ErrorLog.objects.all()
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ErrorLogFilterSet
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return serializers.ErrorLogSerializer
+        return serializers.ErrorLogSerializerList
