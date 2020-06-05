aa8b76c64b8a59d6b3243f9c0edebb9a934dd99f
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-06-05 20:54:28 -0300

    Criando filtros para filtragem de ErrorLog

---
+++ errorcentralapp/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters
+
+from errorcentralapp.models import ErrorLog
+
+
+class ErrorLogFilterSet(filters.FilterSet):
+    level = filters.CharFilter(field_name='level', lookup_expr='exact')
+    source = filters.CharFilter(field_name='source', lookup_expr='icontains')
+    environment = filters.CharFilter(field_name='environment', lookup_expr='in')
+    description = filters.CharFilter(field_name='description', lookup_expr='icontains')
+
+    class Meta:
+        model = ErrorLog
+        fields = ['level', 'environment']
