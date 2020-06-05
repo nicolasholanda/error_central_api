604a446b2eb63093687b5d9724b709785d782dc0
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-06-05 20:52:21 -0300

    Criando serializer sem o campo usuário para ser usado no método post

--- errorcentralapp/serializers.py
+++ errorcentralapp/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from .models import ErrorLog
 
 
-class ErrorLogSerializer(serializers.ModelSerializer):
+class ErrorLogSerializerList(serializers.ModelSerializer):
     class Meta:
         model = ErrorLog
         fields = '__all__'
+
+
+class ErrorLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ErrorLog
+        fields = ['id', 'description', 'source', 'date', 'level', 'environment']
