0afa45d25d1f3f39b6ce5f9ee84b2acb869d430c
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-06-06 10:32:22 -0300

    Correção na lookup_expr para comparar exatamente a string

--- errorcentralapp/filters.py
+++ errorcentralapp/filters.py
@@ -6,7 +6,7 @@ from errorcentralapp.models import ErrorLog
 class ErrorLogFilterSet(filters.FilterSet):
     level = filters.CharFilter(field_name='level', lookup_expr='exact')
     source = filters.CharFilter(field_name='source', lookup_expr='icontains')
-    environment = filters.CharFilter(field_name='environment', lookup_expr='in')
+    environment = filters.CharFilter(field_name='environment', lookup_expr='exact')
     description = filters.CharFilter(field_name='description', lookup_expr='icontains')
 
     class Meta:
